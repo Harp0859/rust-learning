9b1524e093cf038a4656e585c159f60139da85f6
Hariprasad Nivas <110687541+Harp0859@users.noreply.github.com>
2025-06-13 14:45:37 +0530

    Create basic_loops_conditions.rs

---
+++ basic_loops_conditions.rs
@@ -0,0 +1,39 @@
+fn main(){
+let age = 17;
+if age>=18 
+{ println!("he is adult"); }
+else 
+{ println!("he is not an adult"); }
+
+let temp = 60;
+
+if temp==21
+{ println!("wow what a great day"); }
+else if temp <= 21
+{ println!("wow") }
+else
+{ println!("na") }
+
+let is_raining = true;
+let time_of_day = "morning";
+let a = 10;
+let b = 89;
+let c = b/a;
+
+if c < a
+{ println!("result is {}", c); }
+else 
+{ println!("result is {}", b); }
+
+if time_of_day != "night"
+{ println!("its mrng") }
+else if time_of_day == "morning" 
+{ println!(" okay shut") }
+
+if is_raining == false
+{ println!("why no rain?") }
+else
+{ println!("ok wow") }
+
+  
+}
