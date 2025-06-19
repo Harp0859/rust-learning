d7b9562bfc3b82fd9ec357901e31d5c7292b5223
Hariprasad Nivas <110687541+Harp0859@users.noreply.github.com>
2025-06-19 15:00:53 +0530

    Update basic_functions.rs

--- basic_functions.rs
+++ basic_functions.rs
@@ -29,7 +29,58 @@ fn calculate_rectangle_area(l: f64, b: f64)
     println!("area of the rectangle is {}", area);
 }
 
-fn main() { 
+fn get_lucky_num() -> i32 
+{
+    let num = 8;
+    num
+}
+
+fn add_2_num(a: i32, b: i32) -> i32
+{
+    let sum = a+b;
+    sum
+
+}
+
+fn celsius_to_farh(c: f64) -> f64
+{
+
+  let f = (c*1.8)+ 32 as f64;
+  f
+}
+
+fn f_to_c(f: f64) -> f64
+{
+   
+   let c = (f-32 as f64)/1.8;
+   return c
+   
+}
+fn main()
+{
+let c1 = 25.3;
+    println!("The celsius to f for {}`C is {}`F", c1, celsius_to_farh(25.8) );
+    
+    let c2 = 0.0;
+    let f2 = celsius_to_farh(c2);
+    println!("The celsius to f for {}`C is {}`F", c2 ,f2);
+    
+    let f = 98.6;
+    let c3 = f_to_c(f);
+    println!("The f to c for {}'f is {}'c", f, c3);
+    
+    
+    let lucky = get_lucky_num();
+    println!("lucky num is {}", lucky);
+    
+    println!("lucky num is {}",get_lucky_num());
+    
+    let sum_of_2 = add_2_num(1,4);
+    println!("sum of 2 num is {}", sum_of_2);
+    
+    println!("sum of 2 num is {}", add_2_num(1,9));
+    
+
     println!("Starting the program...");
 
     greet_user(); 
